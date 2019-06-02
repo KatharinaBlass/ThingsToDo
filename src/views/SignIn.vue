5108ef485081727cfbf2e99da188fe8c3c39e9e7
Kathi <katharina.blass@student.hpi.de>
2019-06-02 14:40:36 +0200

    sign-in & sign-up ui

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,20 +1,29 @@
 <template>
-    <div>
-        <h2>Sign in to Your Account</h2>
+    <div class="login-wrapper">
+        <span class="login-head-icon">
+            <img src="@/assets/user.png" class="icon"/>
+        </span>
+        <h2 class="login-title">Sign in to Your Account</h2>
         <form v-on:submit.prevent="login">
             <label>
-                Email
-                <input type="email" v-model="email"/>
+                <input type="email" v-model="email" name="email" placeholder="Benutzername" autocomplete="off"/>
             </label>
             <label>
-                Passwort
-                <input type="password" v-model="pw"/>
+                <input type="password" v-model="pw" name="password" placeholder="Passwort" autocomplete="off"/>
             </label>
-            <button type="submit">Sign In</button>
+            <button class="submit-btn" type="submit">Anmelden</button>
         </form>
-        <router-link to="sign-up">
-            Registrierung
-        </router-link>
+        <div class="additional">
+            <div class="sign-up">
+                Registriere dich
+                <router-link to="sign-up" class="sign-up-link">
+                    hier!
+                </router-link>
+            </div>
+            <div class="pw-recovery">
+                Passwort vergessen?
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -42,3 +51,73 @@ export default {
     }
 }
 </script>
+<style lang="scss" scoped>
+.login-wrapper {
+    max-width: 500px;
+    width: 45%;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 1px solid #F1EFEF;
+    padding: 2rem;
+    box-shadow:  0 3px 6px rgba(0, 0, 0, 0.16);
+}
+.login-title{
+    margin-top: 2rem;
+    color: white;
+}
+.submit-btn {
+    width: 100%;
+    margin: 0;
+    padding: .75rem;
+    border: 0;
+    background: var(--accentColor);
+    color: white;
+    border-radius: 2px;
+    text-transform: uppercase;
+    margin-bottom: .5rem;
+}
+input {
+    display: block;
+    width: 100%;
+    padding: 0.75rem;
+    border: none;
+    margin: 1.5rem 0;
+}
+.additional {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    .sign-up, .pw-recovery {
+        color: #F2F2F2;
+        font-style: italic;
+        font-size: 80%;
+        text-align: left;
+
+        .sign-up-link {
+            color: #F2F2F2;
+            font-size: inherit;
+            font-weight: bold;
+        }
+    }
+}
+.login-head-icon {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 24px;
+    border-radius: 50%;
+    border: 1px solid white;
+    width: calc(45px + 48px);
+    height: calc(45px + 48px);
+    background: #36b0b7;
+
+    .icon {
+        height: 45px;
+    }
+}
+</style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,16 +1,17 @@
 <template>
-    <div>
-        <h2>Create an Account</h2>
+    <div class="login-wrapper">
+        <span class="login-head-icon">
+            <img src="@/assets/user.png" class="icon"/>
+        </span>
+        <h2 class="login-title">Create an Account</h2>
         <form v-on:submit.prevent="registrate">
             <label>
-                Email
-                <input type="email" v-model="email"/>
+                <input type="email" v-model="email" name="email" placeholder="Benutzername" autocomplete="off"/>
             </label>
             <label>
-                Passwort
-                <input type="password" v-model="pw"/>
+                <input type="password" v-model="pw" name="password" placeholder="Passwort" autocomplete="off"/>
             </label>
-            <button type="submit">Sign Up</button>
+            <button class="submit-btn" type="submit">Registrieren</button>
         </form>
     </div>
 </template>
@@ -39,3 +40,54 @@ export default {
     }
 }
 </script>
+<style lang="scss" scoped>
+.login-wrapper {
+    max-width: 500px;
+    width: 45%;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 1px solid #F1EFEF;
+    padding: 2rem;
+    box-shadow:  0 3px 6px rgba(0, 0, 0, 0.16);
+}
+.login-title{
+    margin-top: 2rem;
+    color: white;
+}
+.submit-btn {
+    width: 100%;
+    margin: 0;
+    padding: .75rem;
+    border: 0;
+    background: var(--accentColor);
+    color: white;
+    border-radius: 2px;
+    text-transform: uppercase;
+    margin-bottom: .5rem;
+}
+input {
+    display: block;
+    width: 100%;
+    padding: 0.75rem;
+    border: none;
+    margin: 1.5rem 0;
+}
+.login-head-icon {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 24px;
+    border-radius: 50%;
+    border: 1px solid white;
+    width: calc(45px + 48px);
+    height: calc(45px + 48px);
+    background: #36b0b7;
+
+    .icon {
+        height: 45px;
+    }
+}
+</style>
