11596c86d5b168a31c10e0480918d9319bed4972
Kathi <katharina.blass@student.hpi.de>
2019-10-15 20:14:53 +0200

    fix reload bug

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-const firebaseConfig = require('./firebaseConfig.js')
 
 Vue.config.productionTip = false
 
 router.beforeEach((to, from, next) => {
-  const currentUser = firebaseConfig.firebase.auth().currentUser;
+  const currentUser = JSON.parse(localStorage.getItem("user"));
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
   if (requiresAuth && !currentUser) {
     next('/sign-in');
